2ebbe683eaef0fa037ce428c211c46538b3375c5
Eric Matthes <eric@Erics-MacBook-Pro.local>
2020-05-08 20:05:33 -0800

    Makes 300 sample tags, with various levels of nesting.

--- make_sample_tags.py
+++ make_sample_tags.py
@@ -21,7 +21,7 @@ print("Flushed db.")
 
 faker = Faker()
 
-NUM_TAGS = 100
+NUM_TAGS = 300
 
 all_tags = []
 for tag_num in range(NUM_TAGS):
@@ -32,11 +32,11 @@ for tag_num in range(NUM_TAGS):
 print(f"Saved {NUM_TAGS} new tags.")
 
 # Assign last 80% tags one of the first 20% as parents.
-parent_tags = all_tags[:NUM_TAGS//5]
+parent_tags = all_tags[:NUM_TAGS//3]
 child_tags = all_tags[NUM_TAGS//5:]
-assert len(parent_tags) + len(child_tags) == NUM_TAGS
-for ct in child_tags:
-    assert ct not in parent_tags
+# assert len(parent_tags) + len(child_tags) == NUM_TAGS
+# for ct in child_tags:
+#     assert ct not in parent_tags
 
 print('parent_tags:', parent_tags)
 print('child tags:', child_tags)
