5cf65fd79d4dd6b2a5f3da4258ca6d4599f3d20f
Eric Matthes <eric@Erics-MacBook-Pro.local>
2020-05-08 22:40:02 -0800

    200 5ags, with d-a-p, using my all_siblings_leaf(), takes 158 queries.

--- tags/models.py
+++ tags/models.py
@@ -15,10 +15,15 @@ class Tag(MPTTModel, auto_prefetch.Model):
     def __str__(self):
         return self.text
 
-    def all_siblings_leaf(self):
+    def all_siblings_leaf(self, all_tags):
         """Return true if all siblings, including self, are leaf nodes.
         """
-        siblings = self.get_siblings(include_self=True)            
+        # siblings = self.get_siblings(include_self=True)
+        if self.parent is None:
+            siblings = [t for t in all_tags if t.parent is None]
+        else:
+            siblings = [t for t in all_tags if t.parent == self.parent]
+            
         return all([s.is_leaf_node() for s in siblings])
 
 
