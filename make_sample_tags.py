7530f36fa4476331e46e6f1302ed40f81ac51abd
Eric Matthes <eric@Erics-MacBook-Pro.local>
2020-05-08 20:03:52 -0800

    Mostly working.

--- make_sample_tags.py
+++ make_sample_tags.py
@@ -21,7 +21,7 @@ print("Flushed db.")
 
 faker = Faker()
 
-NUM_TAGS = 10
+NUM_TAGS = 100
 
 all_tags = []
 for tag_num in range(NUM_TAGS):
@@ -31,21 +31,27 @@ for tag_num in range(NUM_TAGS):
 
 print(f"Saved {NUM_TAGS} new tags.")
 
-# # Assign last 80% tags one of the first 20% as parents.
-# parent_tags = all_tags[:NUM_TAGS//5]
-# child_tags = all_tags[NUM_TAGS//5:]
-# assert len(parent_tags) + len(child_tags) == NUM_TAGS
-# for ct in child_tags:
-#     assert ct not in parent_tags
+# Assign last 80% tags one of the first 20% as parents.
+parent_tags = all_tags[:NUM_TAGS//5]
+child_tags = all_tags[NUM_TAGS//5:]
+assert len(parent_tags) + len(child_tags) == NUM_TAGS
+for ct in child_tags:
+    assert ct not in parent_tags
 
-# for child_tag in child_tags:
-#     parent_tag = choice(parent_tags)
-#     child_tag.parent = parent_tag
-#     try:
-#         child_tag.save()
-#     except:
-#         print('Exception:', child_tag, parent_tag)
-#         print(parent_tags)
-#         print('done')
+print('parent_tags:', parent_tags)
+print('child tags:', child_tags)
 
-# print("Assigned parents to most tags.")
+for child_tag in child_tags:
+    parent_tag = choice(parent_tags)
+    child_tag.parent = parent_tag
+    try:
+        child_tag.save()
+    except Exception as e:
+        print('Exception: ct, pt', child_tag, parent_tag)
+        print(e)
+        print('done')
+
+print("Assigned parents to most tags.")
+
+Tag.objects.rebuild()
+print("Rebuilt tree.")
